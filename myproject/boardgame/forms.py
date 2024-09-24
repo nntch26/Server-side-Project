edef3fd0ad17cbe2c6de201d62b67ed3d6bc1c3e
65070117Natcha <nntch26@gmail.com>
2024-09-24 12:49:11 +0700

    condition reservation form

--- myproject/boardgame/forms.py
+++ myproject/boardgame/forms.py
@@ -5,7 +5,7 @@ from django import forms
 from .models import *
 
 from datetime import date
-
+from django.utils import timezone
 
 
 class ReservationForm(forms.ModelForm):
@@ -32,3 +32,32 @@ class ReservationForm(forms.ModelForm):
             'reservation_time': 'เวลา',
             'reservation_cap': 'จำนวนคน',
         }
+
+        def clean(self):
+            cleaned_data = super().clean()
+            res_date = cleaned_data.get("reservation_date")
+            res_time = cleaned_data.get("reservation_time")
+            res_cap = cleaned_data.get("reservation_cap")
+            table = cleaned_data.get("table")
+
+            # เช็ควันที่ ต้องไม่เกิน 1 วัน
+            if res_date > date.today()+1 or res_date  < date.today():
+                raise forms.ValidationError("กำหนดเวลาการจองได้ไม่เกิน 1 วัน!")
+            
+            # เช็คเวลา ต้องอยู่ในช่วง 10 โมงถึง 5 ทุ่ม
+            elif res_time < timezone.time(10, 0) or res_time > timezone.time(23, 0) :
+                raise forms.ValidationError("กำหนดเวลาอยู่ในช่วง 10:00 ถึง 23:00!")
+            
+            # เช็คจำนวนคนต้องอยู่ในช่วงจำนวนที่นั่ง
+            elif res_cap:
+                if res_cap <= 0:
+                    raise forms.ValidationError("จำนวนคนต้องมากกว่า 0!")
+
+                if res_cap > table.table_cap:
+                    raise forms.ValidationError(f"จำนวนคนต้องไม่เกิน {table.table_cap} คน!")
+            
+            return cleaned_data
+
+
+        
+
